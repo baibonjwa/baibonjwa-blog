6b3304d3c7c74c157cb04a61ec7bc78f170bd058
BAI-Bonjwa <baibonjwa@gmail.com>
2020-02-24 13:21:46 +0800

    Make article list

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.scss';
+import { useQuery, gql } from '@apollo/client';
+
+const ReactMarkdown = require('react-markdown')
+
+const GET_ARTICLES = gql`
+{
+  articles(last: null) {
+    title
+    content
+    createdAt
+  }
+}
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(GET_ARTICLES);
   return (
     <div className="blog">
       <header className="App-header">
@@ -13,6 +26,22 @@ function App() {
           <li><a href="/search">Search</a></li>
         </ul>
       </header>
+      <ul className="articles">
+        {
+          data && data.articles.map(({
+            title, content, createdAt 
+          } : {
+            title: string, content: string,
+            createdAt: string
+          }) => (
+            <li key={createdAt} className="article">
+              <h2><a href={`/${title}`}>{title}</a></h2>
+              <p className="datetime">{createdAt}</p>
+              <p>{`${content.substring(0, 500)}...`}</p>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient, HttpLink, InMemoryCache, ApolloProvider} from '@apollo/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: process.env.NODE_ENV === "production"
+      ? '/api/graphql' : '/graphql',
+  })
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+  ,document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
